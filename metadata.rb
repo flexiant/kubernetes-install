f9d205534ecbb75e42aaf9d5b3ebf9f5b58892ad
javi@flexiant.com <Javi Perez-Griffo>
2016-01-12 14:42:03 +0000

    First commit empty cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'kubernetes-install'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures kubernetes-install'
+long_description 'Installs/Configures kubernetes-install'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: kubernetes-install
+# Recipe:: default
+#
+# Copyright (C) 2016 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
