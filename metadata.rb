019cbb81a173bb186eeeeabe152c0b2f7629abf4
Javi Perez-Griffo <javi@flexiant.com>
2016-01-13 14:34:19 +0100

    Inclusion of Githooks install script

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'contact@flexiant.com'
 license          'apache2'
 description      'Installs/Configures kubernetes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')).chomp
 
 supports 'ubuntu', '>= 15.04'
 
